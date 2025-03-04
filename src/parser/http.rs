b044ca5b2cc86d7302f10bb9178c09a57ff6f0fe
Akshanabha Chakraborty <ahqsecret@gmail.com>
2025-03-04 18:43:55 +0530

    Auto timeout after 30secs + use HEAD not GET

--- src/parser/http.rs
+++ src/parser/http.rs
@@ -1,15 +1,21 @@
 use reqwest::{Client, ClientBuilder};
 use content_disposition::parse_content_disposition;
 use std::sync::LazyLock;
+use std::time::Duration;
 
-static CLIENT: LazyLock<Client> = LazyLock::new(|| ClientBuilder::new().build().unwrap());
+static CLIENT: LazyLock<Client> = LazyLock::new(|| 
+  ClientBuilder::new()
+    .connect_timeout(Duration::from_secs(30))
+    .build()
+    .unwrap()
+  );
 
 pub async fn cnt_dsp_check(url: &str, ends_with: &str) -> bool {
   cnt_dsp_inner(url, ends_with).await.unwrap_or(false)
 }
 
 async fn cnt_dsp_inner(url: &str, ends: &str) -> Option<bool> {
-  let res = CLIENT.get(url).send().await.ok()?;
+  let res = CLIENT.head(url).send().await.ok()?;
 
   let headers = res.headers();
 
